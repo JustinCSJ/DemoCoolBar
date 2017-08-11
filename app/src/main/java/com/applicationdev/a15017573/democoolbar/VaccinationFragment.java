c7be5e2d06b97324ff707d989b506cb118b7317d
15017573 <15017573@myrp.edu.sg>
2017-08-11 10:45:23 +0800

    Initial commit

---
+++ app/src/main/java/com/applicationdev/a15017573/democoolbar/VaccinationFragment.java
@@ -0,0 +1,34 @@
+package com.applicationdev.a15017573.democoolbar;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+
+ * to handle interaction events.
+ */
+public class VaccinationFragment extends Fragment {
+
+
+
+    public VaccinationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.vaccinationfragment, container, false);
+    }
+
+}
